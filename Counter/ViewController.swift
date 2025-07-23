b731baf596312c0668c842314f6b65c3400e0caf
nar1s <pavelakuz@gmail.com>
2025-07-24 01:55:11 +0300

    Выполнена усложненная задача по спринту 3

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "[dd.MM.yyyy HH:mm:ss]: "
+        return formatter
+    }()
+    
+    @IBOutlet weak var logTextView: UITextView!
+    @IBOutlet weak var deleteButton: UIButton!
+    @IBOutlet weak var decreaseButton: UIButton!
+    @IBOutlet weak var increaseButton: UIButton!
+    @IBOutlet weak var counterLabel: UILabel!
+    private var counter: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        counterLabel.text = "0"
+        increaseButton.tintColor = .red
+        decreaseButton.tintColor = .blue
+        deleteButton.tintColor = .gray
+        logTextView.showsVerticalScrollIndicator = true
+        logTextView.alwaysBounceVertical = true
+        logTextView.text = "История изменений:\n\n"
     }
-
-
+    
+    private func log(_ message: String) {
+        logTextView.text += dateFormatter.string(from: Date()) + message + "\n\n"
+    }
+    
+    @IBAction func setZero(_ sender: Any) {
+        counter = 0
+        counterLabel.text = "0"
+        log("значение сброшено")
+    }
+    @IBAction func decreaseCounter(_ sender: Any) {
+        if counter > 0 {
+            counter -= 1
+            counterLabel.text = String(counter)
+            log("значение изменено на -1")
+        } else {
+            log("попытка уменьшить значение счетчика ниже 0")
+        }
+    }
+    @IBAction func increaseCounter(_ sender: Any) {
+        counter += 1
+        counterLabel.text = String(counter)
+        log("значение изменено на +1")
+    }
 }
 
